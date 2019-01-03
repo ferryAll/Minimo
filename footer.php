2f765048eabb66b856ae8069d78e5bc35ee73d35
Ferry <allan.ferry@orange.fr>
2019-01-03 14:43:32 +0100

    Template installé sur le site

--- footer.php
+++ footer.php
@@ -7,4 +7,10 @@
         <li class="li"><a href= "#" > Tweeter</a></li>
         <li class="li"><a href= "#" > Instagram</a></li>
     </ul>  
+    <script src="assets/js/vendor/jquery.js"></script>
+    <script src="assets/js/vendor/what-input.js"></script>
+    <script src="assets/js/vendor/foundation.js"></script>
+    <script src="assets/js/app.js"></script>
+  </body>
+</html>
 <?php?>
--- header.php
+++ header.php
@@ -1,4 +1,15 @@
 <?php?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+    <link rel="stylesheet" href="assets/css/foundation.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+  </head>
+
     <ul class= "#">
         <li class="li"><a href= "#" > Lifestyle</a></li>
         <li class="li"><a href= "#" > Photodiary</a></li>
