21004868b237ade99c78c39a7c0bee6b5f6a569b
Ferry <allan.ferry@orange.fr>
2019-01-03 10:41:50 +0100

    h

--- header.php
+++ header.php
@@ -1,10 +1,8 @@
 <?php
-<header>
-        <ul class= "lien">
-            <li class="li"><a href= "#" > Lifestyle</a></li>
-            <li class="li"><a href= "#" > Photodiary</a></li>
-            <li class="li"><a href= "#" > Music</a></li>
-            <li class="li"><a href= "#" > Travel</a></li>
-        </ul>  
-    </header>
-    ?>
+    <ul class= "#">
+        <li class="li"><a href= "#" > Lifestyle</a></li>
+        <li class="li"><a href= "#" > Photodiary</a></li>
+        <li class="li"><a href= "#" > Music</a></li>
+        <li class="li"><a href= "#" > Travel</a></li>
+    </ul>  
+?>
