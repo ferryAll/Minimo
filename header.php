e0525ec58a55c7864a3c3c649bb4c63a63220c5d
Ferry <allan.ferry@orange.fr>
2019-01-03 09:51:59 +0100

    peut etre

---
+++ header.php
@@ -0,0 +1,11 @@
+<?php
+<header>
+        <ul class= "lien">
+            <li class="li"><a href= "#" > Home</a></li>
+            <li class="li"><a href= "#" > Service</a></li>
+            <li class="li"><a href= "#" > Clients</a></li>
+            <li class="li"><a href= "#" > Team</a></li>
+            <li class="li"><a href= "#" > Contact</a></li>
+        </ul>  
+    </header>
+    ?>
