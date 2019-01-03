f879480b704bdd425dcc93dcb50894247f387734
Ferry <allan.ferry@orange.fr>
2019-01-03 12:10:03 +0100

    d

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
 <?php?>
     <ul class= "#">
-        <li class="li"><a href= "#" > Lifestyle</a></li>
-        <li class="li"><a href= "#" > Photodiary</a></li>
-        <li class="li"><a href= "#" > Music</a></li>
-        <li class="li"><a href= "#" > Travel</a></li>
+        <li class="li"><a href= "#" > Terms and conditions</a></li>
+        <li class="li"><a href= "#" > Privacy</a></li>
+        Follow:
+        <li class="li"><a href= "#" > Facebook</a></li>
+        <li class="li"><a href= "#" > Tweeter</a></li>
+        <li class="li"><a href= "#" > Instagram</a></li>
     </ul>  
 <?php?>
