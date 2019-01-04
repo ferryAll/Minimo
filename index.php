3e91468ff98ac15e29660928416940d293600883
Ferry <allan.ferry@orange.fr>
2019-01-04 10:01:29 +0100

    mise en place du premier texte(test du css)

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
     include "header.php"
 ?>
-<h3> Photos</h3>
-<br>
-<h5> The perfect weekend getaway </h5>
+<link rel="stylesheet" href="assets/css/css.css">
 
+<h5> Photos</h5>
+<h3> The perfect weekend getaway </h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+<br>Sem viverra aliquet eget sit amet tellus cras adipiscing enim. Ullamcorper sit amet risus nullam eget. 
+<br>Vitae ultricies leo integer malesuada. Sit amet tellus cras adipiscing enim eu turpis egestas pretium. 
+<br>Vulputate ut pharetra sit amet aliquam. Tincidunt id aliquet risus feugiat in ante metus. 
+<br>Consequat nisl vel pretium lectus. A condimentum vitae sapien pellentesque habitant morbi. 
+<br>Tortor condimentum lacinia quis vel eros donec ac odio tempor. Facilisis magna etiam tempor orci eu lobortis. 
+<br>Viverra vitae congue eu consequat ac felis donec. Faucibus purus in massa tempor nec. 
+<br>Viverra ipsum nunc aliquet bibendum.</p>
 <?php
     include "footer.php"
 ?>
