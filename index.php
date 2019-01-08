636eab5c965bdf58db8a13ba7f2545f68bf717e1
Ferry <allan.ferry@orange.fr>
2019-01-08 11:08:01 +0100

    mise en place des fonctionnalités

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
         <div class="cell small-6">
             <img class="main_image" src="assets/images/02_festival.png"><img/>
             <h5> Livestyle</h5>
+            <h3> More than just a music festival </h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             <br> Nunc consequat interdum varius sit amet.
             <br> Ipsum a arcu cursus vitae congue mauris rhoncus.
@@ -31,6 +32,7 @@
         <div class="cell small-6">
             <img class="main_image" src="assets/images/03_caffee.png"><img/>
             <h5> Livestyle</h5>
+            <h3> Life tastes better with coffee </h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             <br> Nunc consequat interdum varius sit amet.
             <br> Ipsum a arcu cursus vitae congue mauris rhoncus.
@@ -42,6 +44,7 @@
         <div class="cell small-6">
             <img class="main_image" src="assets/images/04_pont.png"><img/>
             <h5> Photodiary </h5>
+            <h3> American dream </h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             <br> Nunc consequat interdum varius sit amet.
             <br> Ipsum a arcu cursus vitae congue mauris rhoncus.
@@ -51,6 +54,7 @@
         <div class="cell small-6">
             <img class="main_image" src="assets/images/05_montagnes.png"><img/>
             <h5> Photodiary</h5>
+            <h3> A day exploring the Alps </h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             <br> Nunc consequat interdum varius sit amet.
             <br> Ipsum a arcu cursus vitae congue mauris rhoncus.
@@ -62,7 +66,7 @@
         <div class="newsletter">
             <p>Sign up for our newsletter!</p> 
             <input type="text" placeholder="Enter a valid email address"/>  
-            <input type= "button" placeholder="Submit"/>  
+            <input type= "button" value= "Submit"/>  
         </div>
     </div>
 
@@ -71,6 +75,7 @@
         <div class="cell small-6">
             <img class="main_image" src="assets/images/06_girl.png"><img/>
             <h5> Livestyle </h5>
+            <h3> Top 10 song for running </h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             <br> Nunc consequat interdum varius sit amet.
             <br> Ipsum a arcu cursus vitae congue mauris rhoncus.
@@ -79,6 +84,7 @@
         <div class="cell small-6">
             <img class="main_image" src="assets/images/07_route_glace.png"><img/>
             <h5> Livestyle</h5>
+            <h3> Cold winter days </h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             <br> Nunc consequat interdum varius sit amet.
             <br> Ipsum a arcu cursus vitae congue mauris rhoncus.
