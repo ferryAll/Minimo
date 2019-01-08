ad1f683921f7c92c190adadd56fda666e2159dd7
Ferry <allan.ferry@orange.fr>
2019-01-08 10:01:44 +0100

    mise en place de la page (Version Front End)

--- index.php
+++ index.php
@@ -19,14 +19,75 @@
         <h3><a class="Leave" href= "#" > Leave a comment</a></h3>
     </div>
 
-    <section>
-        <p>Sign up for our newsletter!</p> 
-        <div class="button">
-         <input type="label" placeholder="Enter a valid email address">
-         <button class="input-group-label"> Submit</button>
+    <div class="grid-x">
+        <div class="cell small-6">
+            <img class="main_image" src="assets/images/02_festival.png"><img/>
+            <h5> Livestyle</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            <br> Nunc consequat interdum varius sit amet.
+            <br> Ipsum a arcu cursus vitae congue mauris rhoncus.
+            </p>
         </div>
-    </section> 
+        <div class="cell small-6">
+            <img class="main_image" src="assets/images/03_caffee.png"><img/>
+            <h5> Livestyle</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            <br> Nunc consequat interdum varius sit amet.
+            <br> Ipsum a arcu cursus vitae congue mauris rhoncus.
+            </p>
+        </div>
+    </div>
 
-<?php
+    <div class="grid-x">
+        <div class="cell small-6">
+            <img class="main_image" src="assets/images/04_pont.png"><img/>
+            <h5> Photodiary </h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            <br> Nunc consequat interdum varius sit amet.
+            <br> Ipsum a arcu cursus vitae congue mauris rhoncus.
+            </p>
+        </div>
+
+        <div class="cell small-6">
+            <img class="main_image" src="assets/images/05_montagnes.png"><img/>
+            <h5> Photodiary</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            <br> Nunc consequat interdum varius sit amet.
+            <br> Ipsum a arcu cursus vitae congue mauris rhoncus.
+            </p>
+        </div>
+    </div>
+
+    <div class="grid-x">
+        <div class="newsletter">
+            <p>Sign up for our newsletter!</p> 
+            <input type="text" placeholder="Enter a valid email address"/>  
+            <input type= "button" placeholder="Submit"/>  
+        </div>
+    </div>
+
+
+    <div class="grid-x">
+        <div class="cell small-6">
+            <img class="main_image" src="assets/images/06_girl.png"><img/>
+            <h5> Livestyle </h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            <br> Nunc consequat interdum varius sit amet.
+            <br> Ipsum a arcu cursus vitae congue mauris rhoncus.
+            </p>
+        </div>
+        <div class="cell small-6">
+            <img class="main_image" src="assets/images/07_route_glace.png"><img/>
+            <h5> Livestyle</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            <br> Nunc consequat interdum varius sit amet.
+            <br> Ipsum a arcu cursus vitae congue mauris rhoncus.
+            </p>
+        </div>
+    </div>
+    
+    <button> Load More </button>
+    
+    <?php
     include "footer.php"
-?>
+    ?>
