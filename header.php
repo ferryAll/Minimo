3898c83de2fa8dc30ba6d34807f38cc709e7d343
Ferry <allan.ferry@orange.fr>
2019-01-09 09:25:34 +0100

    changement de place pour le "perfect getaway"

--- header.php
+++ header.php
@@ -20,4 +20,22 @@
         <li class="li"><a href= "#" > Visites</a></li>
     </ul> 
   <img class="main_image" src="assets/images/01_image_principale.png"><img/>
+  
+
+  <div class="grid-x">   
+        <div class="cell">
+            <h5> Photos</h5>
+            <h3> The perfect weekend getaway </h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            <br>Sem viverra aliquet eget sit amet tellus cras adipiscing enim. Ullamcorper sit amet risus nullam eget. 
+            <br>Vitae ultricies leo integer malesuada. Sit amet tellus cras adipiscing enim eu turpis egestas pretium. 
+            <br>Vulputate ut pharetra sit amet aliquam. Tincidunt id aliquet risus feugiat in ante metus. 
+            <br>Consequat nisl vel pretium lectus. A condimentum vitae sapien pellentesque habitant morbi. 
+            <br>Tortor condimentum lacinia quis vel eros donec ac odio tempor. Facilisis magna etiam tempor orci eu lobortis. 
+            <br>Viverra vitae congue eu consequat ac felis donec. Faucibus purus in massa tempor nec. 
+            <br>Viverra ipsum nunc aliquet bibendum.</p>
+        </div>
+        <h3><a class="Leave" href= "#" > Leave a comment</a></h3>
+    </div>
+
 <?php?>
