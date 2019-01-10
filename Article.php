0c1c092b3f0d520696357ce9a094e3adae3aa9bc
Ferry <allan.ferry@orange.fr>
2019-01-10 09:47:34 +0100

    ENORME rectification

--- Article.php
+++ Article.php
@@ -2,8 +2,23 @@
     include "header.php"
 ?>
 
-<div class="grid-x">
-
+<div class="grid-x">   
+    <div class="cell small-8">
+        <h5> Photos</h5>
+        <h3> The perfect weekend getaway </h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        <br>Sem viverra aliquet eget sit amet tellus cras adipiscing enim. Ullamcorper sit amet risus nullam eget. 
+        <br>Vitae ultricies leo integer malesuada. Sit amet tellus cras adipiscing enim eu turpis egestas pretium. 
+        <br>Vulputate ut pharetra sit amet aliquam. Tincidunt id aliquet risus feugiat in ante metus. 
+        <br>Consequat nisl vel pretium lectus. A condimentum vitae sapien pellentesque habitant morbi. 
+        <br>Tortor condimentum lacinia quis vel eros donec ac odio tempor. Facilisis magna etiam tempor orci eu lobortis. 
+        <br>Viverra vitae congue eu consequat ac felis donec. Faucibus purus in massa tempor nec. 
+        <br>Viverra ipsum nunc aliquet bibendum.</p>
+    </div>
+    <div class="cell small-1">
+        <h3>About me</h3>
+        <p>blablablablablablablablablablablabla</p>
+    </div>
 </div>
 <?php
     include "footer.php"
