f615421a0acddba48de19f1659786647540a4a51
Ferry <allan.ferry@orange.fr>
2019-01-11 16:25:32 +0100

    m

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Minimo</title>
     <link rel="stylesheet" href="assets/css/foundation.css">
     <link rel="stylesheet" href="assets/css/app.css">
   </head>
